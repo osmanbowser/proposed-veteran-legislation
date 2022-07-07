91e534ca1fbb0c45a33efffaa7c04db32e3a6210
Osman Bowser <osmanbowser@gmail.com>
2022-07-07 09:39:12 -0500

    setup Axios

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import cheerio from "cheerio";
+import { first } from "cheerio/lib/api/traversing";
+import axios from "axios";
 
-const $ = cheerio.load(`<div><h2 class="primary">First Header</h2><h2>Second Header</h2></div>`);
+const getCongressData =async (): Promise<Array<any>> => {
+	// Get the data
+	const targetUrl = "https://www.congress.gov/search?q=%7B%22congress%22%3A%5B%22117%22%5D%2C%22source%22%3A%22all%22%2C%22search%22%3A%22veteran%22%7D";
+	const pageResponse = await axios.get(targetUrl);
+	
+	//hold results
+	const keys = [];
+	const result = [];
+	//load content to cheerio
+	//scrape the content
+	//return results
+}
